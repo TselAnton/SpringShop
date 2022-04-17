b026238cf84d551ecb29e9c16ce8c6aee0f1b5f2
Anton_Tsel <anton_tsel@epam.com>
2022-04-17 15:53:02 +0400

    - Update checklogs

--- src/main/java/com/tsel/pet/project/spring/shop/data/Product.java
+++ src/main/java/com/tsel/pet/project/spring/shop/data/Product.java
@@ -4,9 +4,14 @@ import java.io.Serial;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,8 +38,9 @@ public class Product implements Serializable {
     @Column
     private String name;
 
-    //todo: прокинуть джойном название
 //    @Column(nullable = false)
+//    @OneToMany
+//    @JoinColumn(name = "category_id", table = "category", referencedColumnName = "id")
 //    private String categoryName;
 
     @Column(nullable = false)
